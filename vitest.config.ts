74f4acdb56d477506d1db002bd68c25cd94d80bd
Hrvoje Pavlinovic <hpavlino@fesb.hr>
2026-06-11 10:40:28 +0200

    Fix Wasp auth mock in Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "wasp/client/auth": "/src/test/waspClientAuth.ts",
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
     include: ["src/**/*.test.{ts,tsx}"],
   },
 });
-
